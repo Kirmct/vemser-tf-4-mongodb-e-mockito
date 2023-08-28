522930aa045f65d612ff9cef2358810cf527fe1e
ricardo <rosastaricardo84@gmail.com>
2023-08-28 16:00:00 -0300

    add: teste usuario service

--- src/main/java/br/com/dbc/vemser/ecommerce/service/UsuarioService.java
+++ src/main/java/br/com/dbc/vemser/ecommerce/service/UsuarioService.java
@@ -27,9 +27,6 @@ public class UsuarioService {
     private final PasswordEncoder bCrypt;
     private final CargoRepository cargoRepository;
 
-//    public Optional<UsuarioEntity> findByLoginAndSenha(String login, String senha) {
-//        return usuarioRepository.findByLoginAndSenha(login, senha);
-//    }
 
     public Optional<UsuarioEntity> findByLogin(String login) {
 
